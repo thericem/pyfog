6e76a35f2e444a9b1e76e235a2c1f6a7f9c1185f
wheelerj <Huts/eu2>
2016-12-23 11:06:43 -0800

    added voltage and frequency properties

--- pyfog/hardware/Agilent_33250A.py
+++ pyfog/hardware/Agilent_33250A.py
@@ -7,3 +7,35 @@ class Agilent_33250A():
 
     def identify(self):
         return self.inst.query('*IDN?')
+
+    @property
+    def freq(self):
+        """Returns frequency in Hz"""
+
+        # get f in kHz
+        f = self.inst.query('FREQ?')
+
+        return float(f) * 1e3
+
+    @freq.setter
+    def freq(self, val):
+        """Sets frequency in Hz"""
+        self.inst.write('FREQ %i' %  (val/1000))
+
+    # alias
+    frequency = freq
+
+    @property
+    def volt(self):
+        """Returns Vpp in volts"""
+        return float(self.inst.query('VOLT?'))
+
+    @volt.setter
+    def volt(self, val):
+        """Sets Vpp in volts"""
+        # debug
+        string = 'VOLT %f' % float(val)
+        self.inst.write(string)
+
+    # alias
+    voltage = volt
