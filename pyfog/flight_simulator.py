ed56dcdecd03d857f469ddf149a8c23bdb1585b0
jondoesntgit <jonathan.m.wheeler@gmail.com>
2017-08-08 10:22:52 -0700

    Tested ARW only for `generate_fog_single`
        rate = 50
        hours = 1
        data1 = cumsum(generate_fog_single(rate=rate, hours=hours, arw=0.0413, drift=0, correlation_time=1800))/rate
        data2 = cumsum(generate_fog_single(rate=rate, hours=hours, arw=0.0413, drift=0, correlation_time=1800))/rate
        data3 = cumsum(generate_fog_single(rate=rate, hours=hours, arw=0.0413, drift=0, correlation_time=1800))/rate
        xdata = arange(0, hours, 1/3600/rate)
        plot(xdata,data1)
        plot(xdata,data2)
        plot(xdata,data3)
        xlabel('Time (h)')
        ylabel('Cumulative Heading (°)')
        title('Simulated cumulative heading with ARW = 0.0413 °/√h')
        savefig('170808a.png')
    
        rate = 10
        hours = 1
        final_points = []
        for i in range(1000):
            final_point = cumsum(generate_fog_single(rate=rate, hours=hours, arw=.0413, drift=0, correlation_time=1800))[-1]/rate
            final_points.append(final_point)
        hist(array(final_points), bins=50);
        xlabel('Final heading error (°)')
        title('Final heading errors for 1000 simulations at ARW=0.0413')
        ylabel('Count')
        savefig('170808b.png')
    
        print(std(final_points))

--- pyfog/flight_simulator.py
+++ pyfog/flight_simulator.py
@@ -6,13 +6,15 @@ and line breaks
 
 import numpy as np
 
-def generate_fog_data(
+
+def generate_fog_single(
         rate=1,  # Hz
         seconds=0,
         minutes=0,
         hours=0,
-        arw=0,  # degree/√h
-        drift=0  # degree/h
+        arw=0,
+        drift=0,
+        correlation_time=1800
         ):
     """Generates a random fog run based on input time and statistics.
 
@@ -33,11 +35,16 @@ def generate_fog_data(
         The angular random walk, specified in degrees per root hour
     drift: float, optional
         The bias drift, specified in degrees per hour
+    correlation_time: float, optional
+        The correlation time in seconds. For more information, see "Stocastic
+        error  simulation method of fiber optic gyros based on performance
+        indicators" by Lv et al. They recommend a value between 1800 and 3600
+        seconds.
 
     Returns
     -------
     ndarray.float
-        An array of data in degrees per hour, whose corresponding index are
+        An array of data in degrees per second, whose corresponding index are
         timestamps whose spacing is determined by the rate parameter.
 
     Raises
@@ -47,13 +54,40 @@ def generate_fog_data(
         If the seconds, minutes, and hours do not add up to a positive time.
     """
 
+    # Define the length in seconds
     time = (hours * 60 * 60
             + minutes * 60
             + seconds)
-
     if time <= 0:
         raise ValueError('Time must be greater than zero')
 
-    a = np.zeros(1)
+    arr_size = int(rate * time)
 
-    return a
+    correlation_time *= rate
+
+    # Set the parameters used by Lv et al
+    Ta = 10 * rate  # 10 seconds
+    ΔT = 1/rate  # sampling time, user-defined
+    qx = drift # bias drift, user-defined
+    Tm = correlation_time #  bias drift, user-defined, default 1800 s
+    qw = arw / 60 / np.sqrt(ΔT) # angular random walk, user-defined, equation 5 in Lv
+    T = time # total time, user-defined
+
+    # Equation 20 in Lv
+    qmw = 0
+#    qmw = np.sqrt((
+#        qx**2 - qw**2/(Ta/ΔT)
+#        )*(
+#            np.pi/2 * (1-np.exp(-2*ΔT/Tm))
+#            /
+#            (np.arctan(np.pi*Tm/Ta) - np.arctan(np.pi*Tm/T))
+#    ))
+
+    markov = np.zeros(arr_size)
+    for i in range(1, arr_size):
+        markov[i] = (np.exp(-rate/correlation_time) * markov[i-1]
+                     + np.random.randn() * qmw)
+
+    noise = np.random.randn(arr_size) * qw
+
+    return noise + markov
